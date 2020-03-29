7bfe123a087820804849b1f3fd2daaab3ed53773
Ksawery Oleszyński <ksawery.oleszynski@gmail.com>
2020-03-29 20:14:13 +0200

    fixes

--- WebApplication/WebApplication/Models/Student.cs
+++ WebApplication/WebApplication/Models/Student.cs
@@ -4,9 +4,10 @@ namespace WebApplication.Models
 {
     public class Student
     {
-        public int IdStudent { get; set; }
+        private static int _counter;
+        public int IdStudent { get; } = _counter++; 
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string IndexNumber  { get; set; } = $"s{new Random().Next(1, 20000)}";
+        public string IndexNumber  { get; } = $"s{new Random().Next(1, 20000)}";
     }
 }
