c4755f3a7c614ec8db627a9f0da4ee78647c2263
Ksawery Oleszyński <ksawery.oleszynski@gmail.com>
2020-05-17 16:18:22 +0200

    added endpoint

---
+++ WebApplication/WebApplication/Models/EnrollmentRequest.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace WebApplication.Models
+{
+    public class EnrollmentRequest
+    {
+        public string IndexNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDate { get; set; }
+        public string Studies { get; set; }
+    }
+}
